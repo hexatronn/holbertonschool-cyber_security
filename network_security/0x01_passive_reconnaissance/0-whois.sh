4887e9e033fe969d95436cd2b2f1f98623ff48af
hexatronn <hikmtmmmdov26@gmail.com>
2026-01-03 21:32:16 +0400

    just

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': *' '/^(Registrant|Admin|Tech) /{sec=$1} /^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Fax|Email)/{f=$2; if($1~/(Street)/)f=f" "; print sec" "substr($1,length(sec)+2)","(f? " "f:"")} /^(Registrant|Admin|Tech) (Phone Ext|Fax Ext)/{print sec" "substr($1,length(sec)+2)","]' > "$1.csv"
+whois "$1" | awk -F': *' '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Fax|Email):/{r=$1; v=$2; sub(/ .*/,"",r); f=substr($1,length(r)+2); if(f=="Street")v=v" "; print r" "f", "v} /^(Registrant|Admin|Tech) (Phone Ext|Fax Ext):/{r=$1; sub(/ .*/,"",r); f=substr($1,length(r)+2); print r" "f","]' > "$1.csv"
