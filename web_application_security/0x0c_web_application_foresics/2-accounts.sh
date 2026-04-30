9fe3f8b4e109309675a07d81a0900343a3423951
hexatronn <hikmtmmmdov26@gmail.com>
2026-04-30 10:46:49 -0400

    added

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep "Accepted" | awk '{print $9}' | sort | uniq
+tail -n 1000 auth.log | awk '/Failed/ {fails[$9]++} /Accepted/ {if (fails[$9] > 3) print $9}' | sort | uniq
